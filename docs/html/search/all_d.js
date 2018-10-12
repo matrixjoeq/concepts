ac28c3bc92ee953cf939ac931e8b7797268e9946
Qu, Xing <xqu@telenav.cn>
2018-10-12 15:13:23 +0800

    Update documents for require_same_type to Same concept switch.

--- docs/html/search/all_d.js
+++ docs/html/search/all_d.js
@@ -5,6 +5,5 @@ var searchData=
   ['regular',['Regular',['../structstl__concept_1_1_regular.html',1,'stl_concept']]],
   ['regular_2ehpp',['regular.hpp',['../regular_8hpp.html',1,'']]],
   ['require_5fexpr_5fconvertible_5fto_2ehpp',['require_expr_convertible_to.hpp',['../require__expr__convertible__to_8hpp.html',1,'']]],
-  ['require_5fsame_5ftype_2ehpp',['require_same_type.hpp',['../require__same__type_8hpp.html',1,'']]],
   ['result_5fof_2ehpp',['result_of.hpp',['../result__of_8hpp.html',1,'']]]
 ];
--- docs/html/search/classes_0.js
+++ docs/html/search/classes_0.js
@@ -5,6 +5,5 @@ var searchData=
   ['_5f_5fis_5freferenceable',['__is_referenceable',['../structstl__concept_1_1____detail_1_1____is__referenceable.html',1,'stl_concept::__detail']]],
   ['_5f_5fis_5fswappable',['__is_swappable',['../structstl__concept_1_1____detail_1_1____is__swappable.html',1,'stl_concept::__detail']]],
   ['_5f_5fis_5fswappable_5fwith',['__is_swappable_with',['../structstl__concept_1_1____detail_1_1____is__swappable__with.html',1,'stl_concept::__detail']]],
-  ['_5f_5fna_5ftype',['__na_type',['../structstl__concept_1_1____detail_1_1____na__type.html',1,'stl_concept::__detail']]],
-  ['_5f_5frequire_5fsame_5ftype',['__require_same_type',['../structstl__concept_1_1____detail_1_1____require__same__type.html',1,'stl_concept::__detail']]]
+  ['_5f_5fna_5ftype',['__na_type',['../structstl__concept_1_1____detail_1_1____na__type.html',1,'stl_concept::__detail']]]
 ];
